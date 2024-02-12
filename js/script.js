693e2f3f6466be6c6a08a57f2f805d3c354fa962
Xgui4 Studio <xgui2004@gmail.com>
2024-02-11 20:31:26 -0500

    ajout des fichiers

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded',init);
+
+function init()
+{	
+    document.getElementById("connexion").addEventListener("click", Message)
+}
+
+function Message(){
+    alert("Le mécanisme de site web dynamique n'est pas supporté par Github Pages.")
+}
